30b3ffc9a032f8d9b73aebf5e7bcf8321bd41529
VimMing <vimmingshe@gmail.com>
2021-01-30 12:19:40 +0800

    chore:设计表结构

---
+++ app/controller/wx.js
@@ -0,0 +1,24 @@
+'use strict';
+const Controller = require('egg').Controller;
+
+class WxController extends Controller {
+
+  async addBirthdayNotice() {
+    const { ctx } = this;
+    ctx.validate({ when: 'date', birthdayId: 'int' });
+    const body = ctx.request.body;
+    ctx.model.WxSubscription.create({
+      userId: ctx.user.id,
+      name: 'birthdayNotice',
+      when: body.when,
+      content: ctx.model.MyFriend.findByPk(body.birthdayId),
+      templateId: 'E3YdVL8G4BZaFJ9ORfp6-nKtRhB1oyh-HWM8zKJpjj8',
+      status: 0
+    })
+      ctx.body = {
+        errCode: 0
+      };
+  }
+}
+
+module.exports = WxController;
---
+++ app/model/WxSubscription.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = app => {
+  const { STRING, INTEGER, DATE, JSON } = app.Sequelize;
+
+  const WxSubscription = app.model.define('WxSubscription', {
+    id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+    userId: {
+      type: INTEGER,
+      field: 'user_id',
+    },
+    name: STRING,
+    when: DATE,
+    content: {
+      type: JSON
+    },
+    status: INTEGER, // 0 , 1 success, 2 fail
+    templateId: {
+      type: STRING,
+      field: 'template_id',
+    },
+    ErrMessage: {
+      type: STRING,
+      field: 'err_message',
+    },
+    createdAt: {
+      type: DATE,
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: DATE,
+      field: 'updated_at',
+    },
+  }, {
+    tableName: 'wx_subscription',
+  });
+
+  return WxSubscription;
+};
