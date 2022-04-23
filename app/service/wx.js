3f7703b27716b914c25b5bc7ed9ffc57978c4caf
VimMing <vimmingshe@gmail.com>
2022-04-23 15:16:31 +0800

    style: 💄 修改utils

--- app/service/wx.js
+++ app/service/wx.js
@@ -1,24 +1,39 @@
-
-
 const Service = require('egg').Service;
 const { Op } = require('sequelize');
-const utils = require('../utils');
+// const utils = require('../utils.ts');
+import { lunarToSolar, formatTime } from '../utils';
 class WxService extends Service {
   async getAccessToken() {
-    const { key: appId, secret: appSecert, host } = this.app.config.passportWeapp;
-    return new Promise(async resolve => {
+    const {
+      key: appId,
+      secret: appSecert,
+      host,
+    } = this.app.config.passportWeapp;
+    return new Promise(async (resolve) => {
       const currentTime = new Date().getTime();
 
-      const redisToken = await this.app.redis.get('wxtoken_token') || '{"access_token": "", "expries_time": 0}';
+      const redisToken =
+        (await this.app.redis.get('wxtoken_token')) ||
+        '{"access_token": "", "expries_time": 0}';
       //   this.ctx.logger.info(redisToken);
       const accessTokenJson = JSON.parse(redisToken);
 
-      if (accessTokenJson.access_token === '' || accessTokenJson.expries_time < currentTime) {
-        const res = await this.ctx.curl(`${host}/cgi-bin/token?appid=${appId}&secret=${appSecert}&grant_type=client_credential`, { dataType: 'json' });
+      if (
+        accessTokenJson.access_token === '' ||
+        accessTokenJson.expries_time < currentTime
+      ) {
+        const res = await this.ctx.curl(
+          `${host}/cgi-bin/token?appid=${appId}&secret=${appSecert}&grant_type=client_credential`,
+          { dataType: 'json' }
+        );
         if (res.data) {
           accessTokenJson.access_token = res.data.access_token;
-          accessTokenJson.expries_time = new Date().getTime() + (parseInt(res.data.expires_in) - 200) * 1000;
-          await this.app.redis.set('wxtoken_token', JSON.stringify(accessTokenJson));
+          accessTokenJson.expries_time =
+            new Date().getTime() + (parseInt(res.data.expires_in) - 200) * 1000;
+          await this.app.redis.set(
+            'wxtoken_token',
+            JSON.stringify(accessTokenJson)
+          );
           resolve(accessTokenJson);
         }
       } else {
@@ -30,13 +45,16 @@ class WxService extends Service {
     const { host } = this.app.config.passportWeapp;
     // 获取access_token
     const tokenJson = await this.getAccessToken();
-    const res = await this.ctx.curl(`${host}/cgi-bin/message/subscribe/send?access_token=${tokenJson.access_token}
-    `, {
-      method: 'POST',
-      contentType: 'json',
-      data: requestData,
-      dataType: 'json',
-    });
+    const res = await this.ctx.curl(
+      `${host}/cgi-bin/message/subscribe/send?access_token=${tokenJson.access_token}
+    `,
+      {
+        method: 'POST',
+        contentType: 'json',
+        data: requestData,
+        dataType: 'json',
+      }
+    );
 
     if (res.data.errmsg === 'ok') {
       await model.update({
@@ -53,8 +71,8 @@ class WxService extends Service {
   async sendBirthdayNotice() {
     const { ctx } = this;
     const now = new Date();
-    const start = utils.formatTime(0, 0, now.getHours());
-    const end = utils.formatTime(59, 59, now.getHours());
+    const start = formatTime(0, 0, now.getHours());
+    const end = formatTime(59, 59, now.getHours());
     const WxSubscription = ctx.model.WxSubscription;
     const User = ctx.model.User;
     User.hasMany(WxSubscription);
@@ -81,24 +99,45 @@ class WxService extends Service {
         let solarBirthday = '';
         if (i.content.isLunar && d) {
           const today = new Date();
-          solarBirthday = utils.lunarToSolar(today.getFullYear(), d.getMonth() + 1, d.getDate());
+          solarBirthday = lunarToSolar(
+            today.getFullYear(),
+            d.getMonth() + 1,
+            d.getDate()
+          );
         } else if (d) {
-          solarBirthday = { year: d.getFullYear(), month: d.getMonth() + 1, day: d.getDate() };
+          solarBirthday = {
+            year: d.getFullYear(),
+            month: d.getMonth() + 1,
+            day: d.getDate(),
+          };
         }
         const distance = Math.floor(
-          ((new Date(solarBirthday.year, solarBirthday.month - 1, solarBirthday.day, 23, 59, 59)).getTime() - (new Date()).getTime()) / (24 * 3600 * 1000)
+          (new Date(
+            solarBirthday.year,
+            solarBirthday.month - 1,
+            solarBirthday.day,
+            23,
+            59,
+            59
+          ).getTime() -
+            new Date().getTime()) /
+            (24 * 3600 * 1000)
         );
         const requestData = {
           touser,
           template_id,
           page,
           data: {
-            time1: { value: `${solarBirthday.year}年${solarBirthday.month}月${solarBirthday.day}日` },
+            time1: {
+              value: `${solarBirthday.year}年${solarBirthday.month}月${solarBirthday.day}日`,
+            },
             thing3: {
               value: name,
             },
             thing2: {
-              value: `距离他的(${i.content.isLunar ? '农历' : '公历'})生日还有${distance}天`,
+              value: `距离他的(${
+                i.content.isLunar ? '农历' : '公历'
+              })生日还有${distance}天`,
             },
           },
         };
